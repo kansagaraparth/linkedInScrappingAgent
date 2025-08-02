90288bfbfdc348e598f3880b9cb3f81c8fd10587
kansagaraparth <kansagaraparth24@gmail.com>
2025-08-02 18:03:48 +0530

    Frontend added

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,22 @@ import  queryHandler from "./routes/llm.route";
 import { ENV_VARS } from "./config/envVars";
 
 const app=express();
+
+// Add CORS middleware to allow frontend requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
-const PORT=ENV_VARS.PORT||5000;
+const PORT=ENV_VARS.PORT||5050;
 
 app.use('/linkedinScrapper',queryHandler);
 app.listen(PORT,()=>{
